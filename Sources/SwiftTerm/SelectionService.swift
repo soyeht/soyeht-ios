soyeht/soyeht-ios

commit 40066b7b7dc0481577094a7efab415889125872b

Only raise the selection changed event if the active state did change

Sources/SwiftTerm/SelectionService.swift
@@ -36,8 +36,8 @@ class SelectionService: CustomDebugStringConvertible {
         set(newValue) {
             if _active != newValue {
                 _active = newValue
+                terminal.tdel?.selectionChanged (source: terminal)
             }
-            terminal.tdel?.selectionChanged (source: terminal)
             if active == false {
                 pivot = nil
             }

commit 8ac0cbc525e1f15df9237d140618f8c6a8205fef

Fix the regression where the selection was no longer being drawn, introduced this regression in 40066b7b7dc0481577094a7efab415889125872b, fixes bug #246

Sources/SwiftTerm/SelectionService.swift
@@ -36,6 +36,10 @@ class SelectionService: CustomDebugStringConvertible {
         set(newValue) {
             if _active != newValue {
                 _active = newValue
+            }
+            // When we set active to true from inside the temrinal, we rely on selectionChanged being raised
+            // because the boundaries changed.
+            if active {
                 terminal.tdel?.selectionChanged (source: terminal)
             }
             if active == false {